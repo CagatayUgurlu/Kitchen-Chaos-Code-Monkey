a73d1972894f1506fb18e492388b55c3465f103b
CagatayUgurlu <cagatayugurlu329@gmail.com>
2024-03-12 13:28:34 +0300

    Cutting Recipe SO and fixed some bugs

--- Assets/[Game]/Scripts/Player.cs
+++ Assets/[Game]/Scripts/Player.cs
@@ -36,6 +36,15 @@ public class Player : MonoBehaviour, IKitchenObjectParent
     private void Start()
     {
         gameInput.OnInteractAction += GameInput_OnInteractAction;
+        gameInput.OnInteractAlternateAction += GameInput_OnInteractAlternateAction;
+    }
+
+    private void GameInput_OnInteractAlternateAction(object sender, EventArgs e)
+    {
+        if (selectedCounter != null)
+        {
+            selectedCounter.InteractAlternate(this);
+        }
     }
 
     private void GameInput_OnInteractAction(object sender, System.EventArgs e)
@@ -112,7 +121,7 @@ public class Player : MonoBehaviour, IKitchenObjectParent
             // Cannot move towards MoveDir
             // Attempt only x movement
             Vector3 moveDirX = new Vector3(moveDir.x, 0f, 0f).normalized;
-            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+            canMove = moveDir.x != 0 && !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
             if (canMove)
             {
                 // Can move only on the X
@@ -123,7 +132,7 @@ public class Player : MonoBehaviour, IKitchenObjectParent
                 // Cannot move only on the X
                 // Attempt only z movement
                 Vector3 moveDirZ = new Vector3(0f, 0f, moveDir.z).normalized;
-                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+                canMove = moveDir.z !=0 && !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
 
                 if (canMove)
                 {

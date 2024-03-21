b35b37ca33c9c62bad583b279005a41c4ac08af3
CagatayUgurlu <cagatayugurlu329@gmail.com>
2024-03-21 12:01:04 +0300

    Game start logic with UI

--- Assets/[Game]/Scripts/Player.cs
+++ Assets/[Game]/Scripts/Player.cs
@@ -42,6 +42,7 @@ public class Player : MonoBehaviour, IKitchenObjectParent
 
     private void GameInput_OnInteractAlternateAction(object sender, EventArgs e)
     {
+        if (!KitchenGameManager.Instance.IsGamePlaying()) return;
         if (selectedCounter != null)
         {
             selectedCounter.InteractAlternate(this);
@@ -50,7 +51,9 @@ public class Player : MonoBehaviour, IKitchenObjectParent
 
     private void GameInput_OnInteractAction(object sender, System.EventArgs e)
     {
-        if(selectedCounter != null)
+        if (!KitchenGameManager.Instance.IsGamePlaying()) return;
+
+        if (selectedCounter != null)
         {
             selectedCounter.Interact(this);
         }

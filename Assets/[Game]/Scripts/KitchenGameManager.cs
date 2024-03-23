6f7cea340a62ad2431fae2baf43cfd00e83d9e59
CagatayUgurlu <cagatayugurlu329@gmail.com>
2024-03-23 15:00:04 +0300

    GamePause and Unpause UI active

--- Assets/[Game]/Scripts/KitchenGameManager.cs
+++ Assets/[Game]/Scripts/KitchenGameManager.cs
@@ -9,6 +9,8 @@ public class KitchenGameManager : MonoBehaviour
     public static KitchenGameManager Instance { get; private set; }
 
     public event EventHandler OnStateChanged;
+    public event EventHandler OnGamePaused;
+    public event EventHandler OnGameUnpaused;
     private enum State
     {
         WaitingToStart,
@@ -108,10 +110,14 @@ public class KitchenGameManager : MonoBehaviour
         if (isGamePaused)
         {
             Time.timeScale = 0f;
+
+            OnGamePaused?.Invoke(this, EventArgs.Empty);
         }
         else
         {
             Time.timeScale = 1f;
+
+            OnGameUnpaused?.Invoke(this, EventArgs.Empty);
         }
     }
 }

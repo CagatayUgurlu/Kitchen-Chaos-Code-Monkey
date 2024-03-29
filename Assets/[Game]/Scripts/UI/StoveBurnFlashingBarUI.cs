839eef1fcd204ab599b2aa4166ffcbe5e92f964e
CagatayUgurlu <cagatayugurlu329@gmail.com>
2024-03-29 13:21:03 +0300

    Stove Burn Warning UI, Timer, Visualize, FlashActive

--- Assets/[Game]/Scripts/UI/StoveBurnFlashingBarUI.cs
+++ Assets/[Game]/Scripts/UI/StoveBurnFlashingBarUI.cs
@@ -2,34 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StoveBurnWarningUI : MonoBehaviour
+public class StoveBurnFlashingBarUI : MonoBehaviour
 {
+    private const string IS_FLASHING = "IsFlashing";
     [SerializeField] private StoveCounter stoveCounter;
 
+    private Animator animator;
 
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
 
     private void Start()
     {
         stoveCounter.OnProgressChanged += StoveCounter_OnProgressChanged;
 
-        Hide();
+        animator.SetBool(IS_FLASHING, false);
+
     }
 
     private void StoveCounter_OnProgressChanged(object sender, IHasProgress.OnProgressChangedEventArgs e)
     {
         float burnShowProgressAmount = .3f;
-        bool show = stoveCounter.IsFried() && e.progressNormalized >= burnShowProgressAmount; 
+        bool show = stoveCounter.IsFried() && e.progressNormalized >= burnShowProgressAmount;
 
-        if(show)
-        {
-            Show();
-        }
-        else
-        {
-            Hide();
-        }
+        animator.SetBool(IS_FLASHING, show);
     }
 
+    /*
     private void Show()
     {
         gameObject.SetActive(true);
@@ -39,4 +40,5 @@ public class StoveBurnWarningUI : MonoBehaviour
     {
         gameObject.SetActive(false);
     }
+    */
 }
